d4ef5e9891eea92612ed26e7e4d75a8fcda895f5
Michael Müller <michael.mueller@sysmagine.com>
2023-12-12 19:18:37 +0100

    Initial commit.

---
+++ moved.py
@@ -0,0 +1,9 @@
+def login_user(db, username, password):
+    user = get_user_by_name(db, username)
+
+    logging.info("User %s logged in", username)
+    if user.password != hash_password(password) or user.disabled or user.expired <= now():
+        raise LoginDenied()
+
+    user.last_login = now()
+    db.commit()
---
+++ reformatted.py
@@ -0,0 +1,8 @@
+def login_user(db, username, password):
+    user = get_user_by_name(db, username)
+
+    if user.password != hash_password(password) or user.disabled or user.expired >= now():
+        raise LoginDenied()
+
+    user.last_login = now()
+    db.commit()
