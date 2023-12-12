ab352a0065cc2cb51bad6cd763d365af9f0d7742
Michael Müller <michael.mueller@sysmagine.com>
2023-12-12 19:21:09 +0100

    evil-merge.py: Add check whether user is expired.

--- evil-merge.py
+++ evil-merge.py
@@ -1,7 +1,7 @@
 def login_user(db, username, password):
     user = get_user_by_name(db, username)
 
-    if user.password != hash_password(password):
+    if user.password != hash_password(password) or user.expired <= now():
         raise LoginDenied()
 
     user.last_login = now()
