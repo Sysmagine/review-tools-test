d3a5e6d452bd1e081c43fa58f7766676de4b15be
Michael Müller <michael.mueller@sysmagine.com>
2023-12-12 22:42:54 +0100

    Add other tests.

---
+++ homoglyphs.js
@@ -0,0 +1,8 @@
+const ip = "127.0.0.1"
+
+// Reject access from localhost
+if (ipǃ="127.0.0.1") {
+	console.log("Go ahead");
+} else {
+	console.log("Access denied!");
+}
---
+++ invisible.js
@@ -0,0 +1,6 @@
+let password = "foo";
+if (password =ㅤ=ㅤ= "bar") {
+	console.log("Access granted");
+} else {
+	console.log("Access denied");
+}
